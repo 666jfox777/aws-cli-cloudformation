43a938c47e087ca5a5bba4f345ad85107d22388b
Justin Fox <jfox@foxcorporations.com>
2015-04-12 12:47:04 -0700

    Add templates and update build script.

--- templates/troposphere/build.sh
+++ templates/troposphere/build.sh
@@ -4,10 +4,15 @@
 #  Usage:  ./build.sh
 #
 # Description:  Compiles troposphere templates into Cloudformation JSON.
-#               Outputs to `../Cloudformation/*.json`.
+#               Outputs to `../cloudformation/*.json`.
 #
 ################################################################################
 
+echo "Converting templates to JSON:"
+
 for filename in ./*.py; do
-    echo $filename
+    echo " -- Processing ${filename}, outputing to ../cloudformation/`echo $filename | tr -d './' | sed 's/..$//'`.json"
+    $filename > ../cloudformation/`echo $filename | tr -d './' | sed 's/..$//'`.json
 done
+
+echo "Conversions complete!"
