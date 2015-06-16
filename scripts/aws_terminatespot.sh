07989b14c2b0d959c5c42b6c8983a10d383e6a29
Justin Fox <666jfox777@users.noreply.github.com>
2015-06-15 17:23:03 -0700

    Create aws_terminatespot.sh

---
+++ scripts/aws_terminatespot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+################################################################################
+#
+#  Usage:  ./aws_terminatespot.sh [profile-name] [region]
+#
+################################################################################
+if [ -z "$1" ]; then
+  echo "Usage: ./aws_terminatespot.sh [profile-name] [region]"
+  exit
+else
+  profile=$1
+fi
+
+if [ -z "$2" ]; then
+  echo "Usage: ./aws_terminatespot.sh [profile-name] [region]"
+  exit
+else
+  region=$2
+fi
+
+instances=`aws ec2 describe-spot-instance-requests --filters Name=state,Values=cancelled --profile ${profile} --region ${region} | jq -c '.SpotInstanceRequests[].InstanceId'`
+
+for instance in $instances
+do
+  aws ec2 terminate-instances --instance-ids ${instance} --profile ${profile} --region ${region} > /dev/null 2>&1
+done
