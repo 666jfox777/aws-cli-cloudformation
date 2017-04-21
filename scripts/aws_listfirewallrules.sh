d948b4234fb5c339599689acc36ad35bfd6e4893
jfox <jfox@Justins-MacBook-Pro.local>
2017-04-20 19:47:18 -0700

    Modified to accept args and use default creds.

--- scripts/aws_listfirewallrules.sh
+++ scripts/aws_listfirewallrules.sh
@@ -12,36 +12,56 @@ usage ()
 {
   echo "Usage: $0 [profile-name] [region] > output.csv"
 }
-if [ -z "$1" ]; then
-  usage
-  exit
-else
-  profile=$1
-fi
 
-if [ -z "$2" ]; then
-  usage
-  exit
+# Process all the arguments
+while getopts ":h?p:r:" opt; do
+    case $opt in
+        p)
+          PROFILE=$OPTARG
+          echo "Account has been set to: $OPTARG" >&2
+          ;;
+        r)
+          REGION=$OPTARG
+          echo "Region has been set to: $OPTARG" >&2
+          ;;
+        h|\?)
+          usage
+          exit 0
+          ;;
+        :)
+          echo "Option -$OPTARG requires an argument." >&2
+          usage
+          exit 1
+          ;;
+    esac
+done
+
+# Set a few defaults
+PROFILE=${PROFILE:-""}
+REGION=${REGION:-"us-east-1"}
+
+if [ -z "$PROFILE" ]; then
+  echo "WARN: As no profile has been set, using default credentials." >&2
 else
-  region=$2
+  PROFILE="$PROFILE"
 fi
 
 echo "Security Group ID, Security Group Name, Security Group Type, Ingress or Egress, Security Group Rule, Protocol, From Port, To Port, CIDR or Security Group ID"
-securitygroups=`aws ec2 describe-security-groups --profile $profile --region $region | jq -c '.SecurityGroups[].GroupId' | tr -d '"' | tr -d ' '`
+securitygroups=`aws ec2 describe-security-groups $PROFILE --region $REGION | jq -c '.SecurityGroups[].GroupId' | tr -d '"' | tr -d ' '`
 for securitygroup in $securitygroups
 do
-  secname=`aws ec2 describe-security-groups --group-ids ${securitygroup} --profile $profile --region $region | jq -c '.SecurityGroups[].GroupName' | tr -d '"' | tr -d ' '`
-  vpcid=`aws ec2 describe-security-groups --group-ids ${securitygroup} --profile $profile --region $region | jq -c '.SecurityGroups[].VpcId' | tr -d '"' | tr -d ' '`
+  secname=`aws ec2 describe-security-groups --group-ids ${securitygroup} $PROFILE --region $REGION | jq -c '.SecurityGroups[].GroupName' | tr -d '"' | tr -d ' '`
+  vpcid=`aws ec2 describe-security-groups --group-ids ${securitygroup} $PROFILE --region $REGION | jq -c '.SecurityGroups[].VpcId' | tr -d '"' | tr -d ' '`
   sectype=""
   if [ "null" != "${vpcid}" ]
   then
-    vpcname=`aws ec2 describe-tags --filters "Name=resource-id,Values=${vpcid}" "Name=key,Values=Name" --profile $profile --region $region | jq -c '.Tags[].Value' | tr -d '"' | tr -d ' '`
+    vpcname=`aws ec2 describe-tags --filters "Name=resource-id,Values=${vpcid}" "Name=key,Values=Name" $PROFILE --region $REGION | jq -c '.Tags[].Value' | tr -d '"' | tr -d ' '`
     sectype="${vpcname} (${vpcid})"
   else
     sectype="Classic"
   fi
-  rulesin=`aws ec2 describe-security-groups --group-ids ${securitygroup} --profile $profile --region $region | jq -c '.SecurityGroups[].IpPermissions[]'`
-  rulesout=`aws ec2 describe-security-groups --group-ids ${securitygroup} --profile $profile --region $region | jq -c '.SecurityGroups[].IpPermissionsEgress[]'`
+  rulesin=`aws ec2 describe-security-groups --group-ids ${securitygroup} $PROFILE --region $REGION | jq -c '.SecurityGroups[].IpPermissions[]'`
+  rulesout=`aws ec2 describe-security-groups --group-ids ${securitygroup} $PROFILE --region $REGION | jq -c '.SecurityGroups[].IpPermissionsEgress[]'`
   for rule in $rulesin
   do
     protocol=`echo "$rule" | jq -c '.IpProtocol' | tr -d '"' | tr -d ' '`
@@ -60,7 +80,7 @@ do
     then
       for sgid in $refsgids
       do
-        sgname=`aws ec2 describe-security-groups --group-ids ${sgid} --profile $profile --region $region 2> /dev/null | jq -c '.SecurityGroups[].GroupName' | tr -d '"' | tr -d ' '`
+        sgname=`aws ec2 describe-security-groups --group-ids ${sgid} $PROFILE --region $REGION 2> /dev/null | jq -c '.SecurityGroups[].GroupName' | tr -d '"' | tr -d ' '`
         if [ "" != "${sgname}" ]
         then
           echo "$securitygroup,$secname,$sectype,Ingress,#,$protocol,$fromport,$toport,$sgname ($sgid)"
@@ -89,7 +109,7 @@ do
     then
       for sgid in $refsgids
       do
-        sgname=`aws ec2 describe-security-groups --group-ids ${sgid} --profile $profile --region $region | jq -c '.SecurityGroups[].GroupName' | tr -d '"' | tr -d ' '`
+        sgname=`aws ec2 describe-security-groups --group-ids ${sgid} $PROFILE --region $REGION | jq -c '.SecurityGroups[].GroupName' | tr -d '"' | tr -d ' '`
         if [ "" != "${sgname}" ]
         then
           echo "$securitygroup,$secname,$sectype,Egress,#,$protocol,$fromport,$toport,$sgname ($sgid)"
